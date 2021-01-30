0f4d35a9a48b35f4462192fd69182b3a42b1570b
Dan Arbelo <govepitr@gmail.com>
2021-01-30 14:13:01 -0700

    working API search

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,83 +1,43 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("searchBtn");
+// // Get the button that opens the modal
+// let btn = document.getElementById("searchBtn");
 
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+
+function userInput(){
+  // Selecting the input element and get its value 
+  var inputVal = document.getElementById("userInput").value;
+   
+    $.ajax({ 
+      url: "https://imdb-api.com/en/API/SearchTitle/k_uc3lp775/" + inputVal,
+      method: 'GET',
+    }).then(function(response) {
+      console.log(response.results);
+      console.log(response);
+      if (response.results) {
+        Array(0) > 1,
+        console.log('Too many results, please be more specific');
+      }
+    });    
+  
+  // Displaying the value
+  console.log(inputVal);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
-// Event handler if the user chooses to hit enter instead of clicking
 $("#userInput").keypress(function(e){
   if(e.which == 13){
     $("#searchBtn").click();
   }
 })
 
-// async function displayMovie() {
 
-
-// //fetching movie posters command
-// var requestOptions = {
-//   method: 'GET',
-//   redirect: 'follow'
-// };
- 
-// fetch("https://imdb-api.com/en/API/Title/k_uc3lp775" + movieTitle, requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-// }
-
-function search (userInput) {
-  
-$.ajax({ 
-  url: "https://imdb-api.com/en/API/SearchTitle/k_uc3lp775/" + userInput,
-  method: 'GET',
-}).then(function(response) {
-  console.log(response.results);
-  console.log(response);
-  if (response.results) {
-    Array(0) > 1,
-    console.log('Too many results, please be more specific');
-  }
-});
-}
-
-
-// errorMessage is the error response for the user's input
-
-
-// function doThis (func) {
-//   func()
-// }
-
-// function doThat () {
-//   return 1
-// }
-
-// search("lordoftherings", doThis, doThat)
 
 // k_uc3lp775
 
-search();
+// search();
