c43bdcf14a747ab16fd75a7f986f6947c9746e54
Dan Arbelo <govepitr@gmail.com>
2021-01-31 12:36:15 -0700

    testing show/hide

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -41,6 +41,34 @@ $("#userInput").keypress(function(e){
   }
 })
 
+var button = userInput();  
+
+$("#searchBtn").click(function (){
+    $(".show").addClass("hide").removeClass("show");
+    $(".hide").addClass("show").removeClass("hide");          
+  
+
+})
+
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+//   console.log(userInput);
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
 
 // function d {
 
