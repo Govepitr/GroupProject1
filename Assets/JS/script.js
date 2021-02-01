a2ebc80f838163467b823b0c200439c0a0c7a044
Dan Arbelo <govepitr@gmail.com>
2021-01-31 22:22:26 -0700

    card centered, parameters all pulling, need to bring to HTML display

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,10 @@
 
 
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-
 function userInput(){
   // Selecting the input element and get its value 
   var inputVal = document.getElementById("userInput").value;
+  
    
     $.ajax({ 
       'url': "http://www.omdbapi.com/?apikey=3171b37b&t=" + inputVal,
@@ -17,32 +14,36 @@ function userInput(){
       var titleResponse = response.Title,
           posterResponse = response.Poster,
           ratedResponse = response.Rated,
-          ratingsResponse = response.Ratings[1].Value,
+          ratingsResponse = response.Ratings[1].Value || "Sorry, your ratings are in another castle!",
           runtimeResponse = response.Runtime,
           genreResponse = response.Genre,
           directorResponse = response.Director,
           plotResponse = response.Plot;
-          console.log(posterResponse, ratedResponse, ratingsResponse, runtimeResponse, genreResponse, directorResponse, plotResponse);
+          console.log(titleResponse, posterResponse, ratedResponse, ratingsResponse, runtimeResponse, genreResponse, directorResponse, plotResponse);
 
       var newCard = document.createElement("div")
           newCard.setAttribute("class", "row")
           newCard.innerHTML = 
-              "<div class=row card-large>" +
-                "<div class=col s12 m7>" +
-                  "<div>" +
-                      `<span class=h1 card-title>${titleResponse}</span>` +
-                        "<div class=card-image>" +
-                          `<img src="/">${posterResponse}</img>` +
-                        "</div>" +
-                  "</div>" +
-                  "<div class='card-content'>" +
-                      `<p>${plotResponse}</p>` +
-                  "</div>" +              
-                "</div> " +
-              "</div>"
-              document.body.appendChild(newCard);
+          "<div class=center-align card>" +
+            "<div class=card-image waves-effect waves-block waves-light>" +
+              `<img src="${posterResponse}">` +
+            "</div>" +
+            "<div class=card-content>" +
+              '<span>${titleResponse}</span>' +
+              '<p>"This is text"</p>'  +
+              '<p>${runtimeResponse}</p>' +
+              '<p>${directorResponse}</p>' +
+              '<p>${genreResponse}</p>' +
+            "</div>" +
+            "<div class=card-reveal>" +
+              '<span>${ratingsResponse[1]}</span>' +
+              '<p id=#plotSpot class=flow-text>${plotResponse}</p>' +
+            "</div>" +
+          "</div>"
+              document.body.appendChild(newCard);              
+              
     });    
-  
+    document.getElementById("userInput").value = "";
    
         
 }
