8b0432e520d5486db41f36e3825903d37ffbd0e2
Dan Arbelo <govepitr@gmail.com>
2021-02-01 20:06:25 -0700

    Huzzah! It's displaying AND dynamic

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -22,41 +22,35 @@ $("#searchBtn").click(function() {
           var titleResponse = response.Title,
               posterResponse = response.Poster,
               ratedResponse = response.Rated,
-              ratingsResponse = response.Ratings[0].Value || response.Ratings[0].Value("Sorry, your ratings are in another castle!"),
+              ratingsResponse = response.Ratings[1].Value || response.Ratings[1].Value("Sorry, your ratings are in another castle!"),
               runtimeResponse = response.Runtime,
               genreResponse = response.Genre,
               directorResponse = response.Director,
               plotResponse = response.Plot;
               console.log(titleResponse, posterResponse, ratedResponse, ratingsResponse, runtimeResponse, genreResponse, directorResponse, plotResponse);
-        
-          document.getElementById("userInput").addEventListener("search", function(event) {
-            $(".movieDisplay2").empty();
-          });
 
-          var newCard = document.createElement("div")          
-              newCard.innerHTML = 
-              `<div id="cardVanish" class="center-align card"> 
-                  <div class="card-image activator waves-effect waves-block waves-light"> 
-                    <img src="${posterResponse}">
-                  </div>` +
-                `<div class="card-content">
-                  <span class="card-title activator blue-text text-darken-4">${titleResponse}</span>
+
+
+
+          document.getElementById("movieDisplay2").innerHTML = 
+            `<div id="cardVanish" class="center-align card"> 
+                <div class="card-image activator waves-effect waves-block waves-light"> 
+                  <img src="${posterResponse}">
                 </div>` +
-                `<div class="card-reveal">
-                  <span class="card-title blue-text text-darken-4">
-                    <p>${directorResponse}</p>
-                    <p>${ratedResponse}</p>
-                    <p>${runtimeResponse}</p>
-                    <p>${ratingsResponse[1]}</p>
-                    <p>${runtimeResponse}</p>
-                  </span>
+              `<div class="card-content">
+            <span class="card-title activator blue-text text-darken-4">${titleResponse}<i class="material-icons right">close</i></span>
+              </div>` +
+              `<div class="card-reveal"><span class="card-title blue-text text-darken-4">
+                  <p>Director: ${directorResponse}</p>
+                  <p>${ratedResponse}</p>
+                  <p>${runtimeResponse}</p>
+                  <p>Ratings: ${ratingsResponse}</p>
+                  <p>${genreResponse}</p>
+                  <p class="line">____________________</P>
                   <p>${plotResponse}</p>
-                </div>
-              </div>` 
-            document.getElementById("movieDisplay2").appendChild(newCard);
-                  
+              </div>
+            </div>` ;
+        document.getElementById("userInput").value = ""    
         });
-
-
 
 });
